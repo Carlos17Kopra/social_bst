bcab8247b74520470ad329b63140e3bfc0fde704
BuildTools <unconfigured@null.spigotmc.org>
2020-09-13 22:53:23 +0200

    NaviUpdate * Header neu designed - Responsiv

--- public/templates/header.php
+++ public/templates/header.php
@@ -5,20 +5,31 @@ require "../_conf/config.php";
 
 //HTML und die Links ausgeben
 echo '
-<link rel="stylesheet" href="../_conf/system/css/style.css" >
-<link rel="stylesheet" href="../_conf/system/scss/master.css" >
-<script src="../_conf/system/js/lib/particles.min.js" defer></script>
-<script src="../_conf/system/js/scripts/headerparticles.js" defer></script>
+<head>
 
-<div class="header" id="particles-js">
+    <meta charset="utf-8">
+    <title>SocialBST</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Infos</a></li>
-            <li><a href="#">Account</a></li>
-            <li><a href="#">FAQ</a></li>
-        </ul>
+    <link rel="stylesheet" href="../_conf/system/css/style.css" >
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-i1LQnF23gykqWXg6jxC2ZbCbUMxyw5gLZY6UiUS98LYV5unm8GWmfkIS6jqJfb4E" crossorigin="anonymous">
 
-    </div>
+</head>
+    <nav>
+      <div class="logo">
+        SocialBST
+      </div>
+<input type="checkbox" id="click">
+      <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+      </label>
+      <ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">Gallery</a></li>
+<li><a href="#">Feedback</a></li>
+</ul>
+</nav>
 
 ';
